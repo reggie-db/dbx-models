a8fb5bd43ad7c0d2cbf398b8db4bcf840a3b0db3
reggie-db <reggie.pierce@databricks.com>
2025-12-16 22:47:47 -0500

    add common

--- notebook_utils.py
+++
@@ -1,8 +0,0 @@
-def widget_get(name, default):
-    try:
-        if value := dbutils.widgets.get(name):
-            return value
-    except Exception:
-        pass
-    dbutils.widgets.text(name, default)
-    return default
